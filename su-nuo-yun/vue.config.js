69f186a55efe52b2200ea61b7eafb22f20482444
GGZZHH <471992509@qq.com>
2019-04-28 17:55:30 +0800

    v1.0

--- su-nuo-yun/vue.config.js
+++ su-nuo-yun/vue.config.js
@@ -1,7 +1,18 @@
+const path = require('path')
+
 module.exports = {
   css: {
     sourceMap: true
   },
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
   lintOnSave: false
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       '@': path.resolve(__dirname, './src')
+  //     }
+  //   }
+  // }
 }
